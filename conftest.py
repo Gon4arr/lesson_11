9483e8d5bd5be06a51ff6f55dcf43e96d3318c69
Nikita Goncharov <goncharovfigma@gmail.com>
2025-05-22 17:35:48 +0300

    fix untils

--- conftest.py
+++ conftest.py
@@ -3,8 +3,33 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selene import Browser, Config
-from utils import attach
+# from utils import attach
 
+import allure
+from allure_commons.types import AttachmentType
+
+
+def add_screenshot(browser):
+    png = browser.driver.get_screenshot_as_png()
+    allure.attach(body=png, name='screenshot', attachment_type=AttachmentType.PNG, extension='.png')
+
+
+def add_logs(browser):
+    log = "".join(f'{text}\n' for text in browser.driver.get_log(log_type='browser'))
+    allure.attach(log, 'browser_logs', AttachmentType.TEXT, '.log')
+
+
+def add_html(browser):
+    html = browser.driver.page_source
+    allure.attach(html, 'page_source', AttachmentType.HTML, '.html')
+
+
+def add_video(browser):
+    video_url = "https://selenoid.autotests.cloud/video/" + browser.driver.session_id + ".mp4"
+    html = "<html><body><video width='100%' height='100%' controls autoplay><source src='" \
+           + video_url \
+           + "' type='video/mp4'></video></body></html>"
+    allure.attach(html, 'video_' + browser.driver.session_id, AttachmentType.HTML, '.html')
 
 @pytest.fixture(scope='function')
 def setup_browser():
@@ -27,10 +52,10 @@ def setup_browser():
     browser.config.driver = driver
     yield browser
 
-    attach.add_screenshot(browser)
-    attach.add_logs(browser)
-    attach.add_html(browser)
-    attach.add_video(browser)
+    add_screenshot(browser)
+    add_logs(browser)
+    add_html(browser)
+    add_video(browser)
 
 
     browser.quit()
